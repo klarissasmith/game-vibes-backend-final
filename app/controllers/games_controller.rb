e761af7d158b959d971a3c5394e3a40ff115a0f2
Klarissa Smith <ksmith_email@yahoo.com>
2020-07-25 22:05:20 -0700

    adding methods to controllers

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,9 +5,12 @@ class GamesController < ApplicationController
   end
 
   def show
-    
+    game = Game.find_by(params[:title])
+    render json: game
   end
 
   private
-  
+    def game_params
+        params.require(:game).permit(:title, :image, :story)
+    end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,4 +3,24 @@ class ReviewsController < ApplicationController
     reviews = Review.all
     render json: reviews
   end
+
+  def show
+    review = Review.find_by(params[:id])
+    render json: review
+  end
+
+  def update
+    review = Review.find_by(params[:id])
+    review.update(review_params)
+    redirect_to review_path(review)
+  end
+
+  def edit
+    review = Review.find_by(params[:id])
+  end
+
+  private
+    def review_params
+      params.require(:review).permit(:summary, :user_id, :game_id)
+    end
 end
